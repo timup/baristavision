1a750453b0119ecdd209f8c682f717d5f1c08aae
Tim Upchurch <mail@timup.ch>
2015-07-28 21:38:10 -0400

    add authentication model, omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  get "/auth/:provider/callback" => "authentications#create"
   devise_for :users
+  resources :authentications
   root 'test#index'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729000614) do
+ActiveRecord::Schema.define(version: 20150729010006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -50,4 +60,5 @@ ActiveRecord::Schema.define(version: 20150729000614) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "authentications", "users"
 end
