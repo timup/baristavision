5def8c1d0750d41e0fd4dd281d27667c37667c5a
Tim Upchurch <mail@timup.ch>
2015-08-05 14:33:32 -0400

    add token authentication

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,29 +5,29 @@ class AuthenticationsController < ApplicationController
   def create
     # Render omniauth hash
     # ----------------------------------------------------
-    # render :text => request.env["omniauth.auth"].to_yaml
+    render :text => request.env["omniauth.auth"].to_yaml
 
     # Create new Authentication or redirect
     # ----------------------------------------------------
-    omniauth = request.env["omniauth.auth"]
-    authentication = current_user.authentication || Authentication.find_by(:provider => omniauth['provider'],
-:uid => omniauth['uid'])
-    if authentication
-      flash[:notice] = "Already authenticated using #{authentication.provider}."
-      sign_in_and_redirect(:user, authentication.user)
-    elsif current_user
-      # auth creation
-      # -------------
-      current_user.create_authentication(:provider => omniauth['provider'],
-:uid => omniauth['uid'])
-      # Move auth creation to model
-      # ---------------------------------------------------
-      # current_user.authentication.create_from_omniauth(omniauth)
-      flash[:notice] = "Authentication successful."
-      redirect_to root_url
-    else
-      redirect_to new_user_registration_url
-    end
+#     omniauth = request.env["omniauth.auth"]
+#     authentication = current_user.authentication || Authentication.find_by(:provider => omniauth['provider'],
+# :uid => omniauth['uid'])
+#     if authentication
+#       flash[:notice] = "Already authenticated using #{authentication.provider}."
+#       sign_in_and_redirect(:user, authentication.user)
+#     elsif current_user
+#       # auth creation
+#       # -------------
+#       current_user.create_authentication(:provider => omniauth['provider'],
+# :uid => omniauth['uid'])
+#       # Move auth creation to model
+#       # ---------------------------------------------------
+#       # current_user.authentication.create_from_omniauth(omniauth)
+#       flash[:notice] = "Authentication successful."
+#       redirect_to root_url
+#     else
+#       redirect_to new_user_registration_url
+#     end
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150802195045) do
+ActiveRecord::Schema.define(version: 20150805182650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,15 @@ ActiveRecord::Schema.define(version: 20150802195045) do
   create_table "authentications", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
+    t.string   "token"
+    t.boolean  "expires"
+    t.datetime "expires_at"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", unique: true, using: :btree
+  add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
