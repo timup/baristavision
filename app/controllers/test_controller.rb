72112b95ad598b7310f0549e2b514aa2009fe078
Tim Upchurch <mail@timup.ch>
2015-08-10 17:02:22 -0400

    test square gem

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,8 @@
 class TestController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    @me = Square::Connect::Merchant.me current_user.authentication.token
+    @payments = @me.payments
   end
 end
