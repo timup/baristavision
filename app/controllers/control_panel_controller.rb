a5e9d08b282f7ed6b411ce221c3fa000ec9d6741
Tim Upchurch <mail@timup.ch>
2015-08-14 14:55:55 -0400

    add control panel controller

---
+++ app/controllers/control_panel_controller.rb
@@ -0,0 +1,13 @@
+class ControlPanelController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'control_panel/index'
+
+  get 'control_panel/new'
+
+  get 'control_panel/create'
+
+  get 'control_panel/destroy'
+
   get "/auth/:provider/callback" => "authentications#create"
   devise_for :users
   resources :authentications
