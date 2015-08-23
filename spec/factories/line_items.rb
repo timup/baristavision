8b93d6a9e59be6dd5da48c27c7d4720db3fe753c
Tim Upchurch <mail@timup.ch>
2015-08-23 14:04:19 -0400

    add order item line_item models

---
+++ spec/factories/line_items.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :line_item do
+    line_item_id "MyString"
+item nil
+order nil
+  end
+
+end
---
+++ spec/factories/orders.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :order do
+    order_id "MyString"
+name "MyString"
+user nil
+  end
+
+end
