4495e60e7e405e0bea57514b077cec1b0a8edabe
Tim Upchurch <mail@timup.ch>
2015-09-02 15:19:20 -0400

    fix sync methods for provider_id change

--- app/models/api/call.rb
+++ app/models/api/call.rb
@@ -31,21 +31,21 @@ module Api
       sync_orders
       orders = Order.where(user_id: @authentication.user.id)
       orders.each do |order|
-        line_items = @connect.line_items(order.order_id)
+        line_items = @connect.line_items(order.provider_order_id)
         api_line_item_ids = []
         if line_items.present?
           line_items.each_with_index do |line_item, index|
             if @provider == 'clover'
-              li = LineItem.where(:line_item_id => line_item.id).first_or_initialize
-              i = Item.find_by(item_id: line_item.item.id)
+              li = LineItem.where(:provider_line_item_id => line_item.id).first_or_initialize
+              i = Item.find_by(provider_item_id: line_item.item.id)
               li.line_item_id = line_item.id
               li.item_id = i.id
               li.order_id = order.id
               li.save!
               api_line_item_ids << line_item.id
             elsif @provider == 'square'
-              li = LineItem.where(:line_item_id => "#{order.order_id}---#{index}").first_or_initialize
-              i = Item.find_by(item_id: line_item.item_detail.item_id)
+              li = LineItem.where(:provider_line_item_id => "#{order.provider_order_id}---#{index}").first_or_initialize
+              i = Item.find_by(provider_item_id: line_item.item_detail.item_id)
               li.item_id = i.try(:id)
               li.order_id = order.id
               li.save!
@@ -68,7 +68,7 @@ module Api
 
         remove_ids = user_line_item_ids - api_line_item_ids
         remove_ids.each do |id|
-          LineItem.where(line_item_id: id).delete_all
+          LineItem.where(provider_line_item_id: id).delete_all
         end
 
       end
@@ -82,9 +82,7 @@ module Api
       api_order_ids = []
       if orders
         orders.each do |order|
-          o = Order.where(:order_id => order.id,
-                          :user_id => @authentication.user.id).first_or_initialize
-          o.order_id = order.id
+          o = Order.where(:provider_order_id => order.id).first_or_initialize
           o.user_id = @authentication.user.id
           o.save!
           api_order_ids << order.id
@@ -94,7 +92,7 @@ module Api
       user_orders = Order.where(user_id: @authentication.user.id)
       @user_order_ids = []
       user_orders.each do |order|
-        @user_order_ids << order.order_id
+        @user_order_ids << order.provider_order_id
       end
 
       remove_ids = @user_order_ids - api_order_ids
@@ -103,7 +101,7 @@ module Api
         # Order.where(order_id: id).each do |order|
         #   LineItem.where(order_id: order.order_id
         # end
-        Order.where(order_id: id).delete_all
+        Order.where(provider_order_id: id).delete_all
       end
     end
 
@@ -116,9 +114,7 @@ module Api
       if items
         items.each do |item|
           # for each item update in db or create new instance
-          i = Item.where(:item_id => item.id,
-                         :name => item.name,
-                         :user_id => @authentication.user.id).first_or_initialize
+          i = Item.where(:provider_item_id => item.id).first_or_initialize
           i.name = item.name
           i.user_id = @authentication.user.id
           i.save!
@@ -130,12 +126,12 @@ module Api
       user_items = Item.where(user_id: @authentication.user.id)
       user_items_ids = []
       user_items.each do |item|
-        user_items_ids << item.item_id
+        user_items_ids << item.provider_item_id
       end
       # set array with all ids to be removed
       remove_ids = user_items_ids - api_item_ids
       remove_ids.each do |id|
-        Item.where(item_id: id).delete_all
+        Item.where(provder_item_id: id).delete_all
       end
     end
 
